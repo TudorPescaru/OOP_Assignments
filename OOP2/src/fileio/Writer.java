ccc1384fd5176b1874bb1d5c29da3e20a983e4ab
Tudor Pescaru <tudorp00@gmail.com>
2020-12-15 12:37:39 +0200

    Adds better json outputting

--- OOP2/src/fileio/Writer.java
+++ OOP2/src/fileio/Writer.java
@@ -1,15 +1,19 @@
 package fileio;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import database.Database;
 import entities.Consumer;
 import entities.Contract;
 import entities.Distributor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import utils.Constants;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This class writes simulation results in JSON format to an output file
@@ -18,10 +22,10 @@ public final class Writer {
     /**
      * The file where the data will be written
      */
-    private final FileWriter file;
+    private final String outFile;
 
-    public Writer(final String path) throws IOException {
-        this.file = new FileWriter(path);
+    public Writer(final String path) {
+        this.outFile = path;
     }
 
     /**
@@ -29,21 +33,21 @@ public final class Writer {
      */
     public void writeOutput() {
         // Create json object to be written
-        JSONObject jsonOutput = new JSONObject();
+        Map<String, Object> jsonOutput = new LinkedHashMap<>();
         // Get array of json output objects for consumers
-        JSONArray jsonConsumers = writeConsumers();
+        List<Map<String, Object>> jsonConsumers = writeConsumers();
         // Add array for consumers to output object
         jsonOutput.put(Constants.CONSUMERS, jsonConsumers);
         // Get array of json output objects for distributors
-        JSONArray jsonDistributors = writeDistributors();
+        List<Map<String, Object>> jsonDistributors = writeDistributors();
         // Add array for distributors to output objects
         jsonOutput.put(Constants.DISTRIBUTORS, jsonDistributors);
         // Write to output file and close
         try {
-            file.write(jsonOutput.toJSONString());
-            file.flush();
-            file.close();
-        } catch (IOException e) {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+            writer.writeValue(Paths.get(outFile).toFile(), jsonOutput);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -52,14 +56,14 @@ public final class Writer {
      * Create array of consumer jsons with data from database
      * @return array of json objects containing consumer data
      */
-    private JSONArray writeConsumers() {
+    private List<Map<String, Object>> writeConsumers() {
         // Create json array for consumers
-        JSONArray jsonConsumers = new JSONArray();
+        List<Map<String, Object>> jsonConsumers = new ArrayList<>();
         Database database = Database.getInstance();
 
         // Convert each consumer object to json object
         for (Consumer consumer : database.getConsumersMap().values()) {
-            JSONObject jsonConsumer = new JSONObject();
+            Map<String, Object> jsonConsumer = new LinkedHashMap<>();
 
             jsonConsumer.put(Constants.ID, consumer.getId());
             jsonConsumer.put(Constants.ISBANKRUPT, consumer.isBankrupt());
@@ -74,21 +78,21 @@ public final class Writer {
      * Create array of distributor jsons with data from database
      * @return array of json objects containing distributor data
      */
-    private JSONArray writeDistributors() {
+    private List<Map<String, Object>> writeDistributors() {
         // Create json array for distributors
-        JSONArray jsonDistributors = new JSONArray();
+        List<Map<String, Object>> jsonDistributors = new ArrayList<>();
         Database database = Database.getInstance();
 
         // Convert each distributor object to json object
         for (Distributor distributor : database.getDistributorsMap().values()) {
-            JSONObject jsonDistributor = new JSONObject();
+            Map<String, Object> jsonDistributor = new LinkedHashMap<>();
 
             jsonDistributor.put(Constants.ID, distributor.getId());
             jsonDistributor.put(Constants.BUDGET, distributor.getBudget());
             jsonDistributor.put(Constants.ISBANKRUPT, distributor.isBankrupt());
 
             // Convert distributor's contract objects to json objects
-            JSONArray jsonContracts = writeContracts(distributor);
+            List<Map<String, Object>> jsonContracts = writeContracts(distributor);
 
             jsonDistributor.put(Constants.CONTRACTS, jsonContracts);
 
@@ -102,13 +106,13 @@ public final class Writer {
      * @param distributor distributor whose contracts are to be converted to json
      * @return array of json objects containing contract data
      */
-    private JSONArray writeContracts(final Distributor distributor) {
+    private List<Map<String, Object>> writeContracts(final Distributor distributor) {
         // Create json array for contracts
-        JSONArray jsonContracts = new JSONArray();
+        List<Map<String, Object>> jsonContracts = new ArrayList<>();
 
         // Convert each of the given distributor's contract objects to json objects
         for (Contract contract : distributor.getContracts()) {
-            JSONObject jsonContract = new JSONObject();
+            Map<String, Object> jsonContract = new LinkedHashMap<>();
 
             jsonContract.put(Constants.CONSUMERID, contract.getConsumer().getId());
             jsonContract.put(Constants.PRICE, contract.getMonthlyCost());
